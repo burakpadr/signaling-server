89a6edd0952713bb5362c2cdf4d2e8a1ca840a2a
burakpadir <burak.padir@intengo.com>
2026-02-09 23:30:09 +0300

    Added call state when peer is online

--- src/main/java/com/burakpadr/signalingserver/service/SessionRegistryService.java
+++ src/main/java/com/burakpadr/signalingserver/service/SessionRegistryService.java
@@ -1,5 +1,7 @@
 package com.burakpadr.signalingserver.service;
 
+import com.burakpadr.signalingserver.constant.EnumCallState;
+import com.burakpadr.signalingserver.context.SessionCallStateContext;
 import com.burakpadr.signalingserver.context.WebSocketSessionContext;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.WebSocketSession;
@@ -9,5 +11,6 @@ public class SessionRegistryService {
 
     public void register(String peerId, WebSocketSession session) {
         WebSocketSessionContext.getInstance().add(peerId, session);
+        SessionCallStateContext.getInstance().add(peerId, EnumCallState.IDLE);
     }
 }
